79d758ec800126d80d42fc793620739f78df8d44
Saifurrahman <saifur13233@gmail.com>
2025-12-20 01:51:02 +0530

    Deleting the task from the task list with the help of their id

--- todo-list/src/TodoList.jsx
+++ todo-list/src/TodoList.jsx
@@ -7,13 +7,22 @@ export default function TodoList() {
 
   let addNewTask = () => {
     //console.log("we have to add new task in todo");
-    setTodos([...todos, { task: newTodo, id: uuidv4() }]);
+    setTodos((prevTodos) => {
+      return [...prevTodos, { task: newTodo, id: uuidv4() }];
+    });
     setNewTodo("");
   };
 
   let updateTodoValue = (event) => {
     //console.log(event.target.value);
     setNewTodo(event.target.value);
+  };
+
+  //for delete any thing in react we use filter method to filter out that thing
+  let deleteTodo = (id) => {
+    // let copy = todos.filter((todo) => todo.id != id);
+    // console.log(copy);
+    setTodos((prevTodos) => todos.filter((prevTodo) => prevTodo.id != id));
   };
 
   return (
@@ -33,7 +42,11 @@ export default function TodoList() {
       <h4>Todo List</h4>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.task}</li>
+          <li key={todo.id}>
+            <span>{todo.task}</span>
+            &nbsp;&nbsp;&nbsp;
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
         ))}
       </ul>
     </div>
